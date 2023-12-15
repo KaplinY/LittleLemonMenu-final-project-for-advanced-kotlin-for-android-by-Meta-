1ee7f0bf12e33e9c6110e2b6413e23b25c40c1ed
Yaroslav <kapliny@mcmaster.ca>
2023-12-14 21:29:06 -0500

    all added

---
+++ app/src/test/java/com/littlelemon/menu/FilterHelperTest.kt
@@ -0,0 +1,18 @@
+package com.littlelemon.menu
+
+import junit.framework.TestCase.assertEquals
+import org.junit.Test
+
+class FilterHelperTest {
+    @Test
+    fun filterProducts_filterTypeDessert_croissantReturned(){
+        val sampleProductsList = mutableListOf(
+            ProductItem(title = "Black tea", price = 3.00, category = "Drinks", R.drawable.black_tea),
+            ProductItem(title = "Croissant", price = 7.00, category = "Dessert", R.drawable.croissant),
+            ProductItem(title = "Bouillabaisse", price = 20.00, category = "Food", R.drawable.bouillabaisse)
+        )
+        val result = FilterHelper().filterProducts(FilterType.Dessert, sampleProductsList)
+        val expected = mutableListOf(ProductItem(title = "Croissant", price = 7.00, category = "Dessert", R.drawable.croissant))
+        assertEquals(result, expected)
+    }
+}
